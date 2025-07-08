bca9957eec2c822d456fdc6b8c8395e1f6159606
ragrawal <ragrawal@cogitate.com>
2025-07-08 23:10:47 +0530

    cors issue fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,12 @@ import googleAuthRoutes from './routes/google.routes'
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://your-frontend-deployment-url.com'],
+    credentials: true, // if you're using cookies or auth headers
+  })
+);
 app.use(express.json());
 app.use(passport.initialize());
 
