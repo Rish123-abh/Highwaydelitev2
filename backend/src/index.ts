b21f34de700681472b388a1e3878ec62392cc63b
ragrawal <ragrawal@cogitate.com>
2025-07-08 23:52:07 +0530

    cors issue fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://your-frontend-deployment-url.com'],
+    origin: ['http://localhost:5173', 'https://subtle-lebkuchen-9a6199.netlify.app'],
     credentials: true, // if you're using cookies or auth headers
   })
 );
